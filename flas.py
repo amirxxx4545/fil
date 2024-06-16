c966347f90b4432ed485cd7856d06ac4979348ed
Amir <khakia799@gmail.com>
2024-06-17 01:01:38 +0330

    newman

---
+++ flas.py
@@ -0,0 +1,19 @@
+from flask import Flask
+
+
+app=Flask(__name__)
+
+@app.route('/')
+def s_name():
+    return "hi my name amir"
+
+
+
+@app.route("/amir")
+def ori_name():
+    return " <h1>say_ name _ali_mohammd</h1>"
+
+if __name__=="__main__":
+    app.run(debug=True)
+    
+    
